d40072432fbc8d609a18411b31b0860d833a778e
tunahan <aslan.tuna.71@gmail.com>
2023-03-10 23:47:03 +0300

    UcakBileti

--- src/javaders/UcakBileti.java
+++ src/javaders/UcakBileti.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class UcakBileti {
     public static void main(String[] args) {
         int km, yas,yolculukTipi;
+        double normalUcret, yasIndirimi, indirimliTutar, geriDonus, toplamTutar;
 
         Scanner input=new Scanner(System.in);
 
@@ -27,51 +28,51 @@ public class UcakBileti {
         switch (yolculukTipi){
             case 1:
                 if(yas<12){
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.50;
-                    double indirimliTutar=normalUcret-yasIndirimi;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.50;
+                    indirimliTutar=normalUcret-yasIndirimi;
                     System.out.println("Toplam tutar: "+indirimliTutar);
                 }else if(yas>=12 && yas<24){
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.10;
-                    double indirimliTutar=normalUcret-yasIndirimi;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.10;
+                    indirimliTutar=normalUcret-yasIndirimi;
                     System.out.println("Toplam tutar: "+indirimliTutar);
                 }else if(yas>=24 && yas<65){
-                    double normalUcret=km*0.10;
+                    normalUcret=km*0.10;
                     System.out.println("Toplam tutar: "+normalUcret);
                 }else{
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.30;
-                    double indirimliTutar=normalUcret-yasIndirimi;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.30;
+                    indirimliTutar=normalUcret-yasIndirimi;
                     System.out.println("Toplam tutar: "+indirimliTutar);
                 }
                 break;
             case 2:
                 if (yas<12){
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.50;
-                    double indirimliTutar=normalUcret-yasIndirimi;
-                    double geriDonus=indirimliTutar*0.20;
-                    double toplamTutar=(indirimliTutar-geriDonus)*2;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.50;
+                    indirimliTutar=normalUcret-yasIndirimi;
+                    geriDonus=indirimliTutar*0.20;
+                    toplamTutar=(indirimliTutar-geriDonus)*2;
                     System.out.println("Toplam tutar: "+toplamTutar);
                 }else if (yas>=12 && yas<24){
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.10;
-                    double indirimliTutar=normalUcret-yasIndirimi;
-                    double geriDonus=indirimliTutar*0.20;
-                    double toplamTutar=(indirimliTutar-geriDonus)*2;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.10;
+                    indirimliTutar=normalUcret-yasIndirimi;
+                    geriDonus=indirimliTutar*0.20;
+                    toplamTutar=(indirimliTutar-geriDonus)*2;
                     System.out.println("Toplam tutar: "+toplamTutar);
                 }else if(yas>=24 && yas<65){
-                    double normalUcret=km*0.10;
-                    double geriDonus=normalUcret*0.20;
-                    double toplamTutar=(normalUcret-geriDonus)*2;
+                    normalUcret=km*0.10;
+                    geriDonus=normalUcret*0.20;
+                    toplamTutar=(normalUcret-geriDonus)*2;
                     System.out.println("Toplam tutar: "+toplamTutar);
                 }else{
-                    double normalUcret=km*0.10;
-                    double yasIndirimi=normalUcret*0.30;
-                    double indirimliTutar=normalUcret-yasIndirimi;
-                    double geriDonus=indirimliTutar*0.20;
-                    double toplamTutar=(indirimliTutar-geriDonus)*2;
+                    normalUcret=km*0.10;
+                    yasIndirimi=normalUcret*0.30;
+                    indirimliTutar=normalUcret-yasIndirimi;
+                    geriDonus=indirimliTutar*0.20;
+                    toplamTutar=(indirimliTutar-geriDonus)*2;
                     System.out.println("Toplam tutar: "+toplamTutar);
                 }
                 break;
