9f7ad2044674d189604191c1d474162ba835f0d4
tunahan <aslan.tuna.71@gmail.com>
2023-03-17 17:23:12 +0300

    update

---
+++ src/diziler/PalindromikKelime.java
@@ -0,0 +1,17 @@
+package diziler;
+
+public class PalindromikKelime {
+
+    static boolean isPalindrome(String str){
+        String reverse="";
+        for (int i=str.length()-1; i>=0; i--){
+            reverse+=str.charAt(i);
+        }
+        return str.equals(reverse);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("hava"));
+    }
+}
